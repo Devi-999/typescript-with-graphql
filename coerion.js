27e55929aca91b0eda5e49a1fb147852b6f1af53
Devi-999 <100109141+Devi-999@users.noreply.github.com>
2024-02-28 13:59:31 +0530

    Add files via upload

---
+++ coerion.js
@@ -0,0 +1,12 @@
+// const a = "1.1" + "1.1"
+// console.log(a)// '1.11.1'
+
+
+// const b = (+"1.1") + (+"1.1"); 
+// console.log(b)
+
+// const a = '100';
+// const b = 100;
+// console.log(Number([10,20]))
+// console.log(10 * [10, 20]) 
+// console.log([1] + [1,2]) 
